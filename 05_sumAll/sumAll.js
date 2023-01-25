4039290f0cc0242921251020a01520b85a408be3
Thomas FitzGerald <tomrfitz@gmail.com>
2023-01-24 19:33:44 -0500

    solve 05 with lots of ifs and a loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+  if (b > a) {
+    for (i = a; i <= b; i++) {
+      sum += i;
+    }
+  } else {
+    for (i = b; i <= a; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
