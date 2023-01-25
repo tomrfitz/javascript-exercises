b26a56bc2e08522bde924e0f25c7bc441de6a0b4
Thomas FitzGerald <tomrfitz@gmail.com>
2023-01-24 19:51:05 -0500

    solve 06 with two if statements

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  let flag = false;
+  if (year % 4 === 0 && year % 100 !== 0) {
+    flag = true;
+  } else if (year % 100 === 0 && year % 400 === 0) {
+    flag = true;
+  }
+  return flag;
 };
 
 // Do not edit below this line
