7c316021ec25de7165d5569a9c457c0bb7fbd787
Thomas FitzGerald <tomrfitz@gmail.com>
2023-01-25 02:06:40 -0500

    solve fibonacci with [1, 1] hard coded

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  let num = parseInt(n);
+  if (num < 0) {
+    return "OOPS";
+  }
+  const fibonacci = [1, 1];
+  for (i = 2; i <= num; i++) {
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+  return fibonacci[num - 1];
 };
 
 // Do not edit below this line
