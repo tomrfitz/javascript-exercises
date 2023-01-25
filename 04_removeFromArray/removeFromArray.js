1835c8547092f79fd54dac03da1bd4d5dbac2d42
Thomas FitzGerald <tomrfitz@gmail.com>
2023-01-24 19:21:48 -0500

    solve 04 with one for loop and .indexOf()

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+  for (let i = 0; i < args.length; i++) {
+    if (array.includes(args[i])) {
+      array.splice(array.indexOf(args[i]), 1); // searches for index of arg to be removed and splices at that idx in original array
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
